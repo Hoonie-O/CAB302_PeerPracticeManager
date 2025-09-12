8e10149d9da8b937ef134efd53d7adf23c1f3c33
Bailey Rossiter <baileyrossiter8@gmail.com>
2025-09-12 17:45:19 +1000

    changed setCurrentUser() to public

--- PeerPractice/src/main/java/com/cab302/peerpractice/Model/UserSession.java
+++ PeerPractice/src/main/java/com/cab302/peerpractice/Model/UserSession.java
@@ -6,7 +6,7 @@ public class UserSession {
 
     public boolean isLoggedIn(){return currentUser != null;}
     public User getCurrentUser(){return currentUser;}
-    void setCurrentUser(User u){this.currentUser = u;}
+    public void setCurrentUser(User u){this.currentUser = u;}
     public void logout(){currentUser = null;}
 
 }
