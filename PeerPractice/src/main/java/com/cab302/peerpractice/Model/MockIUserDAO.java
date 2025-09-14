101775185555f168a9122e875f71209b9b812889
hollyfloweer <n11618230@qut.edu.au>
2025-09-14 18:44:40 +1000

    changed UserDAO to use SQLite

---
+++ PeerPractice/src/main/java/com/cab302/peerpractice/Model/MockIUserDAO.java
@@ -0,0 +1,21 @@
+package com.cab302.peerpractice.Model;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MockIUserDAO {
+    boolean addUser(User user);
+    boolean deleteUser(User user);
+    boolean updateUser(User user);
+    User searchByUsername(String username);
+    List<User> searchByInstitution(String institution);
+    Optional<User> getUserByEmail(String email);
+    Optional<User> getUserByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+    List<User> getAllUsers();
+    boolean storePassword(User user, String hash);
+    String getPassword(User user);
+    boolean addNotification(String username, Notification notification);
+    boolean removeNotification(String username, Notification notification);
+}
